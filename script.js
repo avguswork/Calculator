90afae3dab2b3ddb7cc95f7591d4f29324283d4b
avguswork <99119087+avguswork@users.noreply.github.com>
2022-02-06 17:29:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ function aqual(){
 
                 
         }
-        input.value = result.toFixed(2);
+        input.value = result.toFixed(5);
         console.log(result)
         a = input.value;
         input.focus();
